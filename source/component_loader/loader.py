176e39d2e5e7ed09b7c7cde0a0052a4102ea7350
Jacob Weston-Davis <u7003724@anu.edu.au>
2023-06-25 15:07:11 +1000

    these weren't pushed for some reason

--- source/component_loader/loader.py
+++ source/component_loader/loader.py
@@ -7,11 +7,35 @@ import game_components.ability as ability
 # This will load stuff in the future; for now it's just a file.
 
 
-def load_cape(path: str) -> cape.Cape:
+def load_cape_folder(path: str) -> cape.Cape:
+    """Loads a Cape Folder, complete with abilities
+
+    Args:
+        path (str): the path to the folder to load from.
+
+    Returns:
+        cape.Cape: An object representing the given cape.
+    """
     pass
 
-def load_abilities(paths: List[str]) -> List[ability.Ability]:
+def load_cape(path: str) -> cape.Cape:
+    """Loads a cape from a python class file
+
+    Args:
+        path (str): the path to the file.
+
+    Returns:
+        cape.Cape: the Cape.
+    """
     pass
 
 def load_ability(path: str) -> ability.Ability:
+    """Loads an Ability from a file
+
+    Args:
+        path (str): the path to the file.
+
+    Returns:
+        ability.Ability: the loaded Ability.
+    """
     pass
--- source/game_components/cape.py
+++ source/game_components/cape.py
@@ -1,19 +1,20 @@
 import typing
-from typing import List, Set, Dict, Tuple
+from typing import List, Set, Dict, Tuple, Any
 
 import piece as pc
 import ability as ab
 
 import engine.game_state as game_state
 
-import 
+import hexutils.rbg_hex_functionality as rhex
 
 ## Our capes outline!
 
 class Cape(pc.Piece):
-
-    # Name (This is duplicated here for easy of documentation)
+    # Name/Piece info (This is duplicated here for easy of documentation)
     name: str
+    # Properties/Tags
+    properties: dict[Any]
 
     # Base Stats
     max_health: int
@@ -29,6 +30,10 @@ class Cape(pc.Piece):
     # Ability List
     abilities: List[ab.Ability]
 
+    # Current Field of View
+    # Unfortunately, figures do probably need to know this :ouagh:
+    curr_view: List[rhex.Hex]
+
 
     def __init__(self) -> None:
         super().__init__()
@@ -37,8 +42,46 @@ class Cape(pc.Piece):
 
 
 
-    def get_field_of_view(game_state):
+    # Line of sight, field of view stuff.
+    def generate_field_of_view(self, my_location: rhex.Hex, game_state: game_state.GameState) -> List[rhex.Hex]:
+        """Returns a list of the Hex locations that this Cape can see from a given location
+
+        Args:
+            my_location (rhex.Hex): The location that field of view will be calculated starting from.
+            game_state (game_state.GameState): The current GameState.
+
+        Returns:
+            List[rhex.Hex]: A list of the rhex.Hex locations that this cape can see.
+        """
         pass
 
+    def can_see_location(self, my_location: rhex.Hex, goal_location: rhex.Hex, game_state: game_state.GameState) -> bool:
+        """Checks if we can see a given location from our current location
+        We do this by checking that we can see through all tiles between the two locations
 
+        Args:
+            my_location (rhex.Hex): Our current location.
+            goal_location (rhex.Hex): The location to check if we can see.
+            game_state (game_state.GameState): The current GameState.
+
+        Returns:
+            bool: Whether we can see the goal_location.
+        """
+        pass
+
+    # Abilities
+    # Returns if the ability could successfully be used?
     
+    def use_ability(self, ability: ab.Ability, gamestate: game_state.GameState) -> bool:
+        """
+        This cape uses ability on the gamestate. Returns False if the ability could not be used for whatever reason, True otherwise
+
+        Args:
+            ability (game_components.ability.Ability): The ability object to use. You need to have obtained this already to call this method.
+            gamestate (engine.game_state.GameState): The gamestate to use the ability in.
+
+        Returns:
+            successful (bool): A bool indicating if the Ability was successfully able to be used or not.
+        """
+        pass
+

d2d7fe092656786c991c8aa5b6dcf28ddd8e487a
Jacob Weston-Davis <u7003724@anu.edu.au>
2023-06-25 15:06:51 +1000

    created some more skeletoning functions added some docstrings

--- source/game_components/piece.py
+++ source/game_components/piece.py
@@ -1,5 +1,9 @@
 import typing
-from typing import List, Set, Dict, Tuple
+from typing import List, Set, Dict, Tuple, Any
+
+import engine.game_state as game_state
+
+import hexutils.rbg_hex_functionality as rhex
 
 ## superclass for "Gamepieces"
 ## Doesn't actually need that many things I don't think?
@@ -9,9 +13,42 @@ class Piece:
     # Name
     name: str
 
-    # Tags, for a bunch of uses
-    tags: List[str]
+    # Properties/Tags
+    properties: dict[Any]
+
+    # Owner is a string to allow for weird stuff
+    owner: str
 
 
     def __init__(self) -> None:
+        # Initialising variables
+        self.properties = {}
+        
+
+    
+
+
+
+    # Timing hooks
+    def start_of_turn(gamestate: game_state.GameState) -> None:
+        """This is run at the start of each turn.
+
+        Args:
+            gamestate (game_state.GameState): _description_
+        """
+        pass
+
+    def start_of_main(gamestate: game_state.GameState) -> None:
+        pass
+
+    def end_of_main(gamestate: game_state.GameState) -> None:
+        pass
+
+    def start_of_reaction(gamestate: game_state.GameState) -> None:
+        pass
+
+    def end_of_reaction(gamestate: game_state.GameState) -> None:
+        pass
+
+    def end_of_turn(gamestate: game_state.GameState) -> None:
         pass
